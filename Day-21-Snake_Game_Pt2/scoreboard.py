3bfd3e98f53b3d6eea333b52bd9c86d080313705
Caio Giacon <caiofreire749@gmail.com>
2026-02-01 14:10:54 -0300

    Adicionando o projeto do dia 21

---
+++ Day-21-Snake_Game_Pt2/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+ALIGNMENT = 'center'
+FONT = ('Courier', 10, 'normal')
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.points = 0
+        self.color('white')
+        self.penup()
+        self.goto(x=0, y= 270)
+        self.hideturtle()
+        self.update_score()
+    def update_score(self):
+        self.write(f'Score: {self.points}', align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
+    
+    def increase_score(self):
+        self.points += 1
+        self.clear()
+        self.update_score()
